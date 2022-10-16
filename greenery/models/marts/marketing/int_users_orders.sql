ccb25ae27451f85156d841b0147a5226acec1aa7
danielle <doc22@cornell.edu>
2022-10-16 06:06:58 +0000

    add events to int_users_orders

--- greenery/models/marts/marketing/int_users_orders.sql
+++ greenery/models/marts/marketing/int_users_orders.sql
@@ -5,7 +5,7 @@
 }}
 
 select
-    orders.user_id,
+    orders.user_id as user_id,
     count(orders.order_id) as total_orders,
     sum(orders.order_cost) as total_order_cost,
     sum(orders.shipping_cost) as total_shipping_cost,
@@ -14,7 +14,9 @@ select
     sum(promos.discount) as promo_discounts,
     max(orders.delivered_at) as last_delivery,
     min(orders.created_at) as first_order,
-    max(orders.created_at) as last_order
+    max(orders.created_at) as last_order,
+    count(events.event_id) as total_events
 from {{ref('stg_orders')}} as orders
 left join {{ref('stg_promos')}} as promos on orders.promo_id = promos.promo_id
-group by user_id 
+left join {{ref('stg_events')}} as events on orders.user_id = events.user_id
+group by orders.user_id 
